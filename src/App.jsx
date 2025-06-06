eb4ecef92c68608e418aa70d4560140f055d09a1
Sofia Bergqvist <sofiabergqvist93@gmail.com>
2025-06-06 11:49:35 +0200

    Add button and functionality to start game

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import './index.css'
-import post1 from './assets/POST 1.png'
+import { useState } from 'react'
+import Post from './components/Post.jsx'
 
 function App() {
+
+  const [isGameStarted, setIsGameStarted] = useState(false);
+
+  function goToGame() {
+    setIsGameStarted(true);
+  }
+
+  if(isGameStarted) {
+    return <Post/>
+  }
 
   return (
     <div className='app-container'>
       <h1>Welcome to Alternate Realities</h1>
       <p className='info-text'>Information about the game goes here.</p> 
+      <div className='start-button-container'>
+        <button onClick={goToGame}>Ready to start?</button>
+      </div>
     </div>
   )
 }
