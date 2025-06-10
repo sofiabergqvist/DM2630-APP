9a16094f40cdf47bcafe47d6a84581776ac4519d
Sofia Bergqvist <sofiabergqvist93@gmail.com>
2025-06-10 15:38:29 +0200

    Result tags styling

--- src/components/tags.jsx
+++ src/components/tags.jsx
@@ -2,9 +2,16 @@ import '../index.css'
 
 function Tags({tags}) {  
 
+    const result = tags.map((tag) => <li>{tag}</li>);
+
     return(
         <div className='app-container'>
-            Results: {tags}
+            <h2>Your tags</h2>
+            <div className='result-tags'>
+                <ul>
+                {result}
+                </ul>
+            </div>
         </div>
     );
 }
