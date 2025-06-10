7615ccbc414ff272ed43f6354dde492b7562d674
Sofia Bergqvist <sofiabergqvist93@gmail.com>
2025-06-10 15:20:36 +0200

    Clean up

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Timer from './components/Timer.jsx'
-import Post from './components/Post.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
