b32c6e60c7f17089ceaeb2799aa53eb20347a974
Sofia Bergqvist <sofiabergqvist93@gmail.com>
2025-06-10 15:19:01 +0200

    Create component to show user input tags

---
+++ src/components/tags.jsx
@@ -0,0 +1,12 @@
+import '../index.css'
+
+function Tags({tags}) {  
+
+    return(
+        <div className='app-container'>
+            Results: {tags}
+        </div>
+    );
+}
+
+export default Tags
