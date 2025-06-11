3723d1413d8e8042236fb5e923bc01c5c6de96f4
Sofia Bergqvist <sofiabergqvist93@gmail.com>
2025-06-11 19:27:23 +0200

    Reset timer to 60 sec

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -26,7 +26,7 @@ function Post ({setIsGameStarted}) {
         if(currentPost < 4) {
             setCurrentPost(currentPost+1);
             setIsPosted(false);
-            setTimeLeft(10);
+            setTimeLeft(60);
         }
     }
     
