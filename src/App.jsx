31702c3f8f53061d72390cba0e609e255619d601
Sofia Bergqvist <sofiabergqvist93@gmail.com>
2025-06-11 14:00:31 +0200

    Add info text

--- src/App.jsx
+++ src/App.jsx
@@ -19,9 +19,25 @@ function App() {
         ) : (
           <div className='app-container'>
             <h1>Welcome to Alternate Realities</h1>
-            <p className='info-text'>Information about the game goes here.</p> 
+            <p className='info-text'>
+              This project aims to explore part of the seams of recommendations in social media.               
+            </p> 
+            <p className='info-text'>
+              It is presented as a game, where you will get to see made-up social media posts, and
+              your job is to come up with tags to describe each post (in addition to those
+              that may already be presented in the post caption). 
+            </p>
+            <p className='info-text'>
+              Your friend will see the same set of posts, and should also come up with tags to describe them. 
+              You will have 60 seconds to generate as many tags as possible for the post you are currently seeing, 
+              and then you can each post your set of tags.
+            </p>
+            <p className='info-text'>
+               Once posted, all your tags will be presented to you, and you can compare your set of 
+               tags with that of your friend. Are you living in alternate realities? 
+            </p>
             <div className='start-button-container'>
-              <button onClick={goToGame}>Ready to start?</button>
+              <button onClick={goToGame}>Start</button>
             </div>
           </div>
         )}
