c8e02c11e064092ed798851a5ea31795800ba2a3
Sofia Bergqvist <sofiabergqvist93@gmail.com>
2025-09-10 18:23:17 +0200

    Update info text

--- src/App.jsx
+++ src/App.jsx
@@ -20,16 +20,17 @@ function App() {
           <div className='app-container'>
             <h1>Welcome to Alternate Realities</h1>
             <p className='info-text'>
-              This project aims to explore part of the seams of recommendations in social media.               
+              This project aims to explore part of the seams or edges of filter bubbles by bringing 
+              to light how we as individuals view and interpret social media content.
             </p> 
             <p className='info-text'>
               It is presented as a game, where you will get to see made-up social media posts, and
               your job is to come up with tags to describe each post (in addition to those
-              that may already be presented in the post caption). 
+              that may already be present in the post caption). 
             </p>
             <p className='info-text'>
               Your friend will see the same set of posts, and should also come up with tags to describe them. 
-              You will have 30 seconds to generate as many tags as possible for the post you are currently seeing, 
+              You will have 30 seconds to generate as many tags as possible for the post you are currently viewing, 
               and then you can each post your set of tags.
             </p>
             <p className='info-text'>
